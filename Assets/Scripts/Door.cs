6a6fe553874ab9b0c962f120481d37e865ea3b3d
EstiaanJ <xero416@gmail.com>
2020-02-01 20:14:45 +0800

    Added door breaking
    Added a method to check if the door should break and code to handle that.

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Door : InteractableObject
 {
+    public const float DOOR_MAX_PRESSURE = 800.0f;
+
+
     [SerializeField]
     Sprite open, shut;
     [SerializeField]
@@ -11,6 +14,7 @@ public class Door : InteractableObject
 
     SpriteRenderer _art;
     bool isOpen;
+    bool isBroken;
 
     private void Start()
     {
@@ -19,9 +23,14 @@ public class Door : InteractableObject
 
     public override void Interact()
     {
-        isOpen = !isOpen;
-        _art.sprite = isOpen ? open : shut;
-        gameObject.layer = isOpen ? 9 : 8;
+        if (!isBroken) //Check if the door is broken, if it is not broken the player may interact with it normally, otherwise they may not interact with it and it is locked open.
+        {
+            isOpen = !isOpen;
+
+            _art.sprite = isOpen ? open : shut;
+            gameObject.layer = isOpen ? 9 : 8;
+        }
+
     }
 
     private void Update()
@@ -34,10 +43,26 @@ public class Door : InteractableObject
                 rooms[0].AddAirPressure(-changeInPressure);
                 rooms[1].AddAirPressure(changeInPressure);
             }
-        } else
+        }
+        else
         {
-
+            if (rooms[0].GetDifferenceInAirPressure(rooms[1]) > DOOR_MAX_PRESSURE)
+            {
+                doorBreak();
+                pullPlayer();
+            }
         }
         //print("Air Pressure room 1: " + rooms[1].GetAirPressure());
+    }
+
+    private void doorBreak()
+    {
+        isBroken = true;
+        isOpen = true;
+    }
+
+    private void pullPlayer()
+    {
+    
     }
 }
